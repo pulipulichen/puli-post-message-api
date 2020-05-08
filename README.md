5235783dcc4a8018075170fb45fac7f11335600b
Pulipuli Chen <blog@pulipuli.info>
2020-05-09 02:53:57 +0800

    0509 能在GitHub上跨網域嗎？

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # puli-post-message-api
 PostMessage API
+
+https://pulipulichen.github.io/puli-post-message-api/puli-post-message-api.min.js
+
+icon: https://www.iconninja.com/conversation-circular-symbol-icon-612279
--- puli-post-message-api.js
+++ puli-post-message-api.js
@@ -4,6 +4,12 @@ function PuliPostMessageAPI() {
    * @type type
    */
   let opener
+  if (window.opener) {
+    opener = window.opener
+  }
+  else if (window.parent && window.parent !== window) {
+    opener = window.parent
+  }
 
   let docReady = function (fn) {
     // see if DOM is already available
@@ -15,9 +21,27 @@ function PuliPostMessageAPI() {
     }
   }
   
+  //console.log(opener)
+  if (opener) {
+    docReady(function () {
+      //console.log('ready', location.href.slice(location.href.lastIndexOf('/') + 1))
+      opener.postMessage({
+        eventName: 'ready',
+        url: location.href
+      }, '*')
+      //console.log('ready', location.href.slice(location.href.lastIndexOf('/') + 1))
+    })
+  }
+  
+  let _receiverReadyList = {}
+  let _receiverWaitList = {}
+  
   // -----------------------
   
+  let _receiverElementList = {}
+  
   let send = async function (url, data, options, callback) {
+    url = new URL(url, document.baseURI).href
     if (typeof(options) === 'function' && !callback) {
       callback = options
       options = undefined
@@ -29,11 +53,15 @@ function PuliPostMessageAPI() {
     }
     
     let receiver
-    if (mode === 'iframe') {
-      receiver = document.createElement("iframe"); 
-      receiver.style.display = 'none'
-      receiver.src = url
-      document.body.appendChild(receiver)
+    let receiverElement
+    if (_receiverElementList[url]) {
+      receiver = _receiverElementList[url]
+    }
+    else if (mode === 'iframe') {
+      receiverElement = document.createElement("iframe"); 
+      receiverElement.style.display = 'none'
+      receiverElement.src = url
+      document.body.appendChild(receiverElement)
     }
     else if (mode === 'popup') {
       let target = undefined
@@ -48,20 +76,49 @@ function PuliPostMessageAPI() {
       receiver = window.open(url, target, features)
     }
     
-    let result = await _sendToReceiver(receiver, url, data)
+    // ----------------
     
     let autoClose = true
-    if (options && options.autoClose) {
-      autoClose = options.autoClose
+    if (options) {
+      if (options.autoClose === false) {
+        autoClose = options.autoClose
+      }
+      else if (options.mode === 'popup') {
+        autoClose = false
+      }
     }
+    
+    
+    // ---------------
+    let getReceiver = function () {
+      if (!receiver && mode === 'iframe') {
+        receiver = receiverElement.contentWindow
+      } 
+      return receiver
+    }
+    
+    let result = await _sendToReceiver(getReceiver, url, data)
+    
+    if (autoClose === false) {
+      if (!_receiverElementList[url]) {
+        _receiverElementList[url] = receiver
+      }
+    }
+    
+    // ---------------
     
     if (autoClose === true) {
       if (mode === 'iframe') {
-        receiver.parentNode.removeChild(receiver)
+        receiverElement.parentNode.removeChild(receiverElement)
       }
       else {
         receiver.close()
       }
+      delete _receiverReadyList[url]
+    }
+    
+    if (typeof(callback) === 'function') {
+      callback(result)
     }
     
     return result
@@ -69,23 +126,138 @@ function PuliPostMessageAPI() {
   
   // -----------------------
   
-  let _sendToReceiver = async function (receiver, url, data) {
-    await waitReceiverReady(url)
+  let _sendToReceiver = async function (getReceiver, url, data) {
+    await _waitReceiverReady(url)
     
+    let receiver = getReceiver()
+    //console.log(receiver)
     receiver.postMessage({
       eventName: 'send',
       data: data,
+      url: location.href
     }, url)
     
-    let result = await waitReceiverReturn(url)
-    return result
+    return new Promise(function (resolve, reject) {
+      _pushReceiverReturnQueue(url, function (result) {
+        resolve(result)
+      })
+    })
+    
   }
+  
+  let _receiverReturnQueue = {}
+  
+  let _pushReceiverReturnQueue = function (url, callback) {
+    if (!_receiverReturnQueue[url]) {
+      _receiverReturnQueue[url] = []
+    }
+    if (_receiverReturnQueue[url].indexOf(callback) === -1) {
+      _receiverReturnQueue[url].push(callback)
+    }
+  }
+  
+  
+  // ------------------------
+  
+  let _messageHandler = function (event) {
+    //console.log(event, location.href.slice(location.href.lastIndexOf('/') + 1))
+    if (typeof(event.data) !== 'object'
+            || !event.data.eventName) {
+      return false
+    }
+    let eventName = event.data.eventName
+    let data = event.data.data
+    let url = event.data.url
+    
+    //console.log(eventName, data, location.href)
+    
+    //let origin = event.origin
+    let source = event.source
+    //let url = event.source.location.href
+    
+    if (eventName === 'return') {
+      _returnEventHandler(url, data)
+    }
+    else if (eventName === 'send') {
+      _sendEventHandler(source, url, data)
+    }
+    else if (eventName === 'ready') {
+      _readyEventHandler(url)
+    }
+  }
+  
+  let _returnEventHandler = function (origin, data) {
+    //console.log(_receiverReturnQueue)
+    if (Array.isArray(_receiverReturnQueue[origin]) === false) {
+      return false
+    }
+
+    _receiverReturnQueue[origin].forEach(function (callback) {
+      if (typeof(callback) !== 'function') {
+        return false
+      }
+      callback(data)
+    })
+
+    // 清空呼叫的資料
+    _receiverReturnQueue[origin] = []
+    return true
+  }
+  
+  let _sendEventHandler = async function (source, origin, input) {
+    let result
+    
+    if (typeof(_receiveHandler) === 'function') {
+      result = await _receiveHandler(input)
+    }
+    
+    source.postMessage({
+      eventName: 'return',
+      data: result,
+      url: location.href
+    }, origin)
+  }
+  
+  let _readyEventHandler = function (origin) {
+    _receiverReadyList[origin] = true
+    //console.log(_receiverWaitList[origin], origin, location.href)
+    if (typeof(_receiverWaitList[origin]) === 'function') {
+      _receiverWaitList[origin]()
+      delete _receiverWaitList[origin]
+    }
+  }
+  
+  let _waitReceiverReady = function (url) {
+    //console.log(_receiverReadyList[url], _receiverWaitList[url])
+    if (_receiverReadyList[url] === true) {
+      return true
+    }
+    
+    return new Promise(function (resolve, reject) {
+      _receiverWaitList[url] = function () {
+        resolve(true)
+      }
+      //console.log(_receiverReadyList[url], _receiverWaitList[url], url)
+    })
+  }
+  
+  window.addEventListener("message", _messageHandler, true)
   
   // -----------------------
   
+  
+  // -----------------------
+  
+  let _receiveHandler
+  
   let onReceive = function (callback) {
-    
+    if (typeof(callback) !== 'function') {
+      return false
+    }
+    _receiveHandler = callback
   }
+  
+  // -------------------------------
   
   return {
     send: send,
