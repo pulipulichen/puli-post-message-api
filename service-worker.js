230b4a7eb780dbb93140e4af710ec9a3c603555d
Pulipuli Chen <blog@pulipuli.info>
2021-06-13 19:02:04 +0800

    0613 加入了上傳檔案的方式

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const RUNTIME = 'runtime';
-const PRECACHE = 'precache-v2021-0613-111209'
+const PRECACHE = 'precache-v2021-0613-181409'
       
 /**
  * How to build cache list?
